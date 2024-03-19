ceb70d89890280c8dab5b2f426d0b0a401726b1e
Ali Khaleghi <awli.khaleghi@gmail.com>
2024-03-20 02:54:02 +0330

    half way into history page, also added Setting page and 3 options with it

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 from time import sleep
 from config import config, saveSetting
-from history import get_history, add_history
+from history import get_history, add_history, delete_history
 from start_listening import listen, speak
 import flet as ft
 import pyperclip
@@ -14,7 +14,8 @@ def main(page: ft.Page):
   page.title = "Voice To Text (AK)"
   page.window_width = 400
   page.window_height = 600
-  page.window_always_on_top = True
+  if(config.get('Settings', 'alwaysontop') == 'active'):
+    page.window_always_on_top = True
   page.window_resizable = False
   # page.window_frameless = True
   page.window_maximizable = False
@@ -48,6 +49,11 @@ def main(page: ft.Page):
   else:
     darkmodeIcon = ft.icons.WB_SUNNY_OUTLINED
 
+  def set_on_top(e):
+    config.set('Settings', 'alwaysontop', 'active' if AlwaysOnTopCheck.value else 'no')
+    page.window_always_on_top = AlwaysOnTopCheck.value
+    saveSetting()
+    page.update()
   def set_lang(e):
     config.set('Settings', 'language', dd.value)
     saveSetting()
@@ -118,18 +124,19 @@ def main(page: ft.Page):
   whatISaid = ft.TextField( 
     multiline=True, 
     read_only=True, 
-    min_lines=10,
-    max_lines=10,
+    min_lines=12,
+    max_lines=12,
     # value=config.get('Settings', "language"),
   )
 
   whatISaid.label = 'What You Said:' if whatISaid.value else 'Please click Start listening and talk.'
 
-  c1 = ft.Switch(label="AutoCopy",on_change=set_auto_copy, value=True if config.get('Settings', 'autocopy') == 'active' else False)
 
 
   def close_yes_dlg(e):
     page.close_dialog()
+    print(dlg)
+    print(dlg.data)
     dlg.data.confirm_dismiss(True)
 
   def close_no_dlg(e):
@@ -151,23 +158,27 @@ def main(page: ft.Page):
     if e.direction == ft.DismissDirection.END_TO_START: # right-to-left slide
       # save current dismissible to dialog's data
       dlg.data = e.control
+      print(e.page)
       page.show_dialog(dlg)
     else: # left-to-right slide
       e.control.confirm_dismiss(True)
 
   def handle_dismiss(e):
+    print(history.controls[0].text)
     # history.controls.remove(e.control)
     page.update()
 
   def handle_update(e: ft.DismissibleUpdateEvent):
-    print(f"Update - direction: {e.direction}, progress: {e.progress}, reached: {e.reached}, previous_reached: {e.previous_reached}")
+    e
+    # print(f"Update - direction: {e.direction}, progress: {e.progress}, reached: {e.reached}, previous_reached: {e.previous_reached}")
   
   items = get_history()
   page.update()
   history = ft.ListView(
     controls=[
       ft.Dismissible(
-        content=ft.ListTile(title=ft.Text(f"Item {i}")),
+        data=i[0],
+        content=ft.ListTile( title=ft.Text(f"{i[1]}")),
         dismiss_direction=ft.DismissDirection.HORIZONTAL,
         background=ft.Container(bgcolor=ft.colors.GREEN),
         secondary_background=ft.Container(bgcolor=ft.colors.RED),
@@ -182,7 +193,28 @@ def main(page: ft.Page):
       for i in items
     ],
     expand=True,
-  ) 
+  )
+
+  ## settings 
+
+  AlwaysOnTop = ft.CupertinoListTile(
+    leading=(AlwaysOnTopCheck:=ft.Checkbox(label="", value=True if config.get('Settings', 'alwaysontop') == 'active' else False,on_change=set_on_top)) ,
+    title=ft.Text("On-Top"),
+    subtitle=ft.Text("Keep app Always On Top."),
+  )
+  AutoCopy = ft.CupertinoListTile(
+    leading=ft.Checkbox(label="", value=True if config.get('Settings', 'autocopy') == 'active' else False,on_change=set_auto_copy) ,
+    title=ft.Text("Auto-Copy"),
+    subtitle=ft.Text("Whether or not to auto-copy messages."),
+  )
+  ThemeMode = ft.CupertinoListTile(
+    leading=ft.Checkbox(label="", value=True if config.get('Settings', 'theme') == 'dark' else False, on_change=switch_theme_mode) ,
+    title=ft.Text("Night-Mode"),
+    subtitle=ft.Text("Use the Dark Theme."),
+    
+  )
+
+
   def updateTabs(e): 
     page.update()
 
@@ -212,10 +244,6 @@ def main(page: ft.Page):
                   height=50, 
                   border_radius=ft.border_radius.all(5),
                 )
-              ]),
-              ft.Column(
-              [
-                c1,
               ]) 
             ],spacing=10,alignment=ft.MainAxisAlignment.CENTER))
             
@@ -230,7 +258,11 @@ def main(page: ft.Page):
       ft.Tab(
         text="Setting",
         icon=ft.icons.SETTINGS,
-        content=ft.IconButton(darkmodeIcon, on_click=switch_theme_mode),
+        content=ft.Column([
+          AlwaysOnTop,
+          AutoCopy,
+          ThemeMode
+        ]),
       ),
     ],
     expand=1,
