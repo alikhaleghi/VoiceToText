92c4fa862178372dbe40fb55f0cf00d7cff7fc6a
Ali Khaleghi <awli.khaleghi@gmail.com>
2024-03-19 23:14:00 +0330

    manage history

---
+++ history.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def connect():
+    # Connect
+    conn = sqlite3.connect('history.sqlite', check_same_thread=False)
+    # Create Table
+    conn.execute("CREATE TABLE IF NOT EXISTS messages ( id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT )")
+    return conn
+
+
+# Get History
+def get_history():
+    conn = connect()
+    v = conn.execute("select * from messages order by id desc")
+    history = v.fetchall()
+    conn.close()
+    return history
+
+# Add History
+def add_history(text):
+    conn = connect()
+    conn.execute("insert into messages values(?,?)",(None, text,))
+    conn.commit()
+    conn.close()
